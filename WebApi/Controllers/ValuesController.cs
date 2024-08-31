b04e08fbae81ef9a1ee375f55f3ad2b14afc08d2
Syedhashim-ali012 <hashimsyed077@gmail.com>
2024-08-31 16:53:13 +0500

    abc

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly AdreinContext context;
+
+        public ValuesController(AdreinContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public JsonResult list()
+        {
+            var show = context.Users.ToList();
+
+			return new JsonResult(Ok(show));
+		}
+        [HttpPost]
+         public JsonResult create(User user)
+        {
+            context.Users.Add(user);
+			context.SaveChanges();
+
+			return new JsonResult(Ok("User Successfully Registered"));
+		}
+	}
+    }
+
---
+++ WebApi/Models/AdreinContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApi.Models
+{
+    public partial class AdreinContext : DbContext
+    {
+        public AdreinContext()
+        {
+        }
+
+        public AdreinContext(DbContextOptions<AdreinContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("User");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone).HasColumnName("phone");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
